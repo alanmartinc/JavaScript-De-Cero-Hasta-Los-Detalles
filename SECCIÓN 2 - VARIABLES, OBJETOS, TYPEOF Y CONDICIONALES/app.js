26cd3467edf26fad6770f4da1804636c1cde1dd3
alanmartinc <alancabot.ac@gmail.com>
2020-10-19 18:37:01 -0300

    Se agrega ejercicios escritura dinamica - tipos primitivos

---
+++ SECCIÓN 2 - VARIABLES, OBJETOS, TYPEOF Y CONDICIONALES/app.js
@@ -0,0 +1,27 @@
+var num = 10;
+var str = "texto";
+var bol = true;
+var und = undefined;
+var nul = null;
+
+console.log(num);
+
+num = str;
+
+console.log(num);
+
+bol = 10;
+
+console.log(bol);
+
+var obj = {
+    numero: 10,
+    texto: "Nuevo texto",
+
+    objHijo: {
+        numero2: 20,
+        texto2: "Nuevo texto 2"
+    }
+};
+
+console.log(obj);
