7af6b2d8abecfc23a6a2637be00f34f22f925ec5
alanmartinc <alancabot.ac@gmail.com>
2020-10-21 20:43:38 -0300

    Se agrega ejercicios funciones de primera clase

---
+++ SECCIÓN 2 - VARIABLES, OBJETOS, TYPEOF Y CONDICIONALES/FUNCIONES DE PRIMERA CLASE/app.js
@@ -0,0 +1,15 @@
+function a(){
+    console.log("Funcion a");
+}
+
+a();
+
+a.nombre = "Maria";
+a.direccion = {
+    pais: "Costa Rica",
+    ciudad: "San José",
+    edificio: {
+        piso: "8vo",
+        nombre: "Edificio principal"
+    }
+}
