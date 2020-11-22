d391f4e4a185dcc0cd54060126187ba2ff5bc9a0
alanmartinc <alancabot.ac@gmail.com>
2020-11-21 23:53:35 -0300

    Se agrega ejercicios Switch condicional multiple

---
+++ SECCIÓN 4 - INTERPRETACION, PALABRAS RESERVADAS, DECISIONES, ESCAPE Y COOKIES/app7.js
@@ -0,0 +1,18 @@
+var mes = undefined;
+
+switch(mes){
+    case(10>5)? 40:1 :
+    console.log("Febrero");
+    break;
+
+    case undefined:
+    console.log("Febrero");
+    break;
+
+    case 3:
+    console.log("Marzo");
+    break;
+
+    default:
+    console.log("Cualquier otro mes");
+}
