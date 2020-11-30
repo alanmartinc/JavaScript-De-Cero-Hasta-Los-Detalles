3fbf7deb2acdb6b0afb503a3f227ed3ca9e78dba
alanmartinc <alancabot.ac@gmail.com>
2020-11-30 12:19:02 -0300

    Se agrega ejercicios JavaScript - use stric - modo estricto

---
+++ SECCIÓN 6 - EVENTOS/app4.js
@@ -0,0 +1,13 @@
+(function(){
+    "use stric";
+
+    function getNombre(){
+        nombre2 = "Juan";
+        return nombre2;
+    }
+
+    var nombre = "Fernando";
+
+    console.log(nombre);
+    console.log(getNombre());
+})();
