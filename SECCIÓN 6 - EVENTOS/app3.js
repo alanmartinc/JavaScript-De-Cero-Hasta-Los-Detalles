a25d6e01d99d08af926909d5f7d32af395ce004d
alanmartinc <alancabot.ac@gmail.com>
2020-11-30 12:15:05 -0300

    Se agrega ejercicios cajas de dialogo

---
+++ SECCIÓN 6 - EVENTOS/app3.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+    console.log("Segundo...");
+},1000)
+
+var nombre = prompt("Ingrese su nombre","nombre");
+
+console.log(nombre);
+
+// Alertas: https://sweetalert.js.org/
